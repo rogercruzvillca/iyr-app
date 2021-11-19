03e2282108af884d8ac1c6c13bb4e68da35b6a61
Roger Cruz Villca <rogercruzvillca@gmail.com>
2021-11-19 15:22:33 -0400

    zincronizy packeje.json XIV

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/IyR'));
+app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/IyR/'}),
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
 );
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
